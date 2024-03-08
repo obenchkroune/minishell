d2fa6acf0c461651045c8da76d77a6f755117849
oussama benchkron said <obenchkr@student.1337.ma>
2024-03-08 11:04:01 +0000

    move readline imports to minishell.h

---
+++ includes/minishell.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 22:57:01 by obenchkr          #+#    #+#             */
+/*   Updated: 2024/03/08 10:30:40 by obenchkr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+
+# define MINISHELL_H
+
+
+# define RED     "\x1b[31m"
+# define GREEN   "\x1b[32m"
+# define YELLOW  "\x1b[33m"
+# define BLUE    "\x1b[34m"
+# define MAGENTA "\x1b[35m"
+# define CYAN    "\x1b[36m"
+# define RESET   "\x1b[0m"
+
+#include <stdio.h>
+#include <readline/history.h>
+#include <readline/readline.h>
+#include <libft.h>
+
+#endif
