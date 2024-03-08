2b1cba06617a8bffbea3ff4ad441a04489d065b5
oussama benchkron said <obenchkr@student.1337.ma>
2024-03-08 11:06:27 +0000

    testing stuff

--- src/main.c
+++ src/main.c
@@ -1,11 +1,52 @@
-#include <readline/history.h>
-#include <readline/readline.h>
-#include <stdio.h>
+#include "minishell.h"
+#include "lexer.h"
 
-int	main(int argc, char const *argv[])
+void	print_tokens(t_token *tokens);
+void	handle_history(const char *input, t_token *token);
+
+int	main(int argc, char const *argv[], char **envp)
 {
+	char	*input;
+	t_token	*tokens;
+
 	(void)argc;
 	(void)argv;
-	printf("Hello");
+	(void)envp; // for now
+	while (1)
+	{
+		input = readline("minishell$ ");
+		tokens = tokenize(input);
+		handle_history(input, tokens);
+		print_tokens(tokens);
+		free_tokens(tokens);
+		free(input);
+	}
 	return (0);
+}
+
+void	print_tokens(t_token *tokens)
+{
+	t_token *current = tokens;
+
+	if (!has_valid_syntax(tokens))
+	{
+		printf("Invalid Syntax!\n");
+		return ;
+	}
+	while (current != NULL)
+	{
+		printf("Type: %d, Value: %s\n", current->type, current->value);
+		current = current->next;
+	}
+}
+
+void	handle_history(const char *input, t_token *token)
+{
+	while (token)
+	{
+		if (token->type == T_HEREDOC)
+			return ;
+		token = token->next;
+	}
+	add_history(input);
 }
