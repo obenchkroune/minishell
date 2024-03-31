8d97e7307eb9e41da0545bc6d5d33886fbfdc203
Yacoub <yaharkat@student.1337.ma>
2024-03-31 13:09:57 +0000

    fix(builtins): this is not javascript, fixed some builtins comp code

--- src/exec/exec_builtins.c
+++ src/exec/exec_builtins.c
@@ -6,7 +6,7 @@
 /*   By: yaharkat <yaharkat@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 10:12:37 by yaharkat          #+#    #+#             */
-/*   Updated: 2024/03/30 10:59:30 by yaharkat         ###   ########.fr       */
+/*   Updated: 2024/03/31 13:08:47 by yaharkat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,18 @@ bool	ft_is_builtin(char *cmd)
 
 void	ft_exec_builtin(char **args)
 {
-	if (args[0] == "echo")
+	if (ft_strncmp(args[0], "echo", 4) == 0)
 		ft_echo(args);
-	else if (args[0] == "cd")
+	else if (ft_strncmp(args[0], "cd", 2) == 0)
 		ft_cd(args);
-	else if (args[0] == "pwd")
+	else if (ft_strncmp(args[0], "pwd", 3) == 0)
 		ft_pwd();
-	else if (args[0] == "export")
+	else if (ft_strncmp(args[0], "export", 6) == 0)
 		ft_export(args);
-	else if (args[0] == "unset")
+	else if (ft_strncmp(args[0], "unset", 5) == 0)
 		ft_unset(args);
-	else if (args[0] == "env")
+	else if (ft_strncmp(args[0], "env", 3) == 0)
 		ft_env();
-	else if (args[0] == "exit")
+	else if (ft_strncmp(args[0], "exit", 4) == 0)
 		ft_exit();
 }
