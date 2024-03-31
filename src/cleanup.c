8531f4f890a1c87ba5eccb924c0398975a005f7a
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2024-03-31 09:13:39 +0100

    set variables to NULL after free

--- src/cleanup.c
+++ src/cleanup.c
@@ -6,7 +6,7 @@
 /*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 22:52:15 by obenchkr          #+#    #+#             */
-/*   Updated: 2024/03/31 08:43:11 by obenchkr         ###   ########.fr       */
+/*   Updated: 2024/03/31 09:10:14 by obenchkr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,15 @@
 void	cleanup_shell(void)
 {
 	free_env(g_shell->env);
+	g_shell->env = NULL;
 	free(g_shell);
+	g_shell = NULL;
 }
 
 void	cleanup_rotation(void)
 {
 	free_tree(g_shell->nodes);
+	g_shell->nodes = NULL;
 	free(g_shell->input);
+	g_shell->input = NULL;
 }
