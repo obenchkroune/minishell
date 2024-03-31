72cbcd9339060039e6ec91379a11f7fad1296c6b
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2024-03-31 09:12:59 +0100

    let's not talk about it 😂

--- src/exec/exec_builtins.c
+++ src/exec/exec_builtins.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_builtins.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaharkat <yaharkat@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 10:12:37 by yaharkat          #+#    #+#             */
-/*   Updated: 2024/03/30 10:59:30 by yaharkat         ###   ########.fr       */
+/*   Updated: 2024/03/31 09:08:37 by obenchkr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,38 @@
 
 bool	ft_is_builtin(char *cmd)
 {
-	if (!ft_strncmp(cmd, "echo", ft_strlen(cmd)) || !ft_strncmp(cmd, "cd",
-			ft_strlen(cmd)) || !ft_strncmp(cmd, "pwd", ft_strlen(cmd))
-		|| !ft_strncmp(cmd, "export", ft_strlen(cmd)) || !ft_strncmp(cmd,
-			"unset", ft_strlen(cmd)) || !ft_strncmp(cmd, "env", ft_strlen(cmd))
-		|| !ft_strncmp(cmd, "exit", ft_strlen(cmd)))
-		return (true);
+	const char	*builtins[] = {
+		"echo",
+		"cd",
+		"pwd",
+		"export",
+		"unset",
+		"env",
+		"exit"
+	};
+
+	while (*builtins)
+	{
+		if (!ft_strcmp(cmd, *builtins))
+			return (true);
+	}
 	return (false);
 }
 
 void	ft_exec_builtin(char **args)
 {
-	if (args[0] == "echo")
+	if (ft_strcmp(args[0], "echo") == 0)
 		ft_echo(args);
-	else if (args[0] == "cd")
+	else if (ft_strcmp(args[0], "cd") == 0)
 		ft_cd(args);
-	else if (args[0] == "pwd")
+	else if (ft_strcmp(args[0], "pwd") == 0)
 		ft_pwd();
-	else if (args[0] == "export")
+	else if (ft_strcmp(args[0], "export") == 0)
 		ft_export(args);
-	else if (args[0] == "unset")
+	else if (ft_strcmp(args[0], "unset") == 0)
 		ft_unset(args);
-	else if (args[0] == "env")
+	else if (ft_strcmp(args[0], "env") == 0)
 		ft_env();
-	else if (args[0] == "exit")
+	else if (ft_strcmp(args[0], "exit") == 0)
 		ft_exit();
 }
