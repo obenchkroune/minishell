451cb16cd895cc2309c2f52d7e60d240ea5d9b79
obenchkr <obenchkr@student.1337.ma>
2024-04-04 05:11:27 +0000

    fix segfault due to use after free

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaharkat <yaharkat@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 09:30:04 by yaharkat          #+#    #+#             */
-/*   Updated: 2024/04/04 00:53:01 by yaharkat         ###   ########.fr       */
+/*   Updated: 2024/04/04 05:10:24 by obenchkr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 void	ft_exit(void)
 {
+	int	status;
+
+	status = g_shell->last_exit_status;
 	cleanup_rotation();
 	cleanup_shell();
-	exit(g_shell->last_exit_status);
+	exit(status);
 }
