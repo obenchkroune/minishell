973b15b2b9bcebd1175b1707e41b623c8ed6ff48
Yacoub <yaharkat@student.1337.ma>
2024-04-04 04:32:05 +0000

    norm: made redirect function shorter

--- src/exec/redirect.c
+++ src/exec/redirect.c
@@ -6,11 +6,55 @@
 /*   By: yaharkat <yaharkat@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 03:40:45 by yaharkat          #+#    #+#             */
-/*   Updated: 2024/04/04 03:49:16 by yaharkat         ###   ########.fr       */
+/*   Updated: 2024/04/04 04:31:31 by yaharkat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+static void	redirect_in(t_io *io)
+{
+	int	fd;
+
+	fd = open(io->file, O_RDONLY);
+	if (fd == -1)
+		panic("open");
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+}
+
+static void	redirect_out(t_io *io)
+{
+	int	fd;
+
+	fd = open(io->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd == -1)
+		panic("open");
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+}
+
+static void	redirect_append(t_io *io)
+{
+	int	fd;
+
+	fd = open(io->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (fd == -1)
+		panic("open");
+	dup2(fd, STDOUT_FILENO);
+	close(fd);
+}
+
+static void	redirect_heredoc(t_io *io)
+{
+	int	fd;
+
+	fd = open("to do", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd == -1)
+		panic("open");
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+}
 
 int	ft_redirect(t_io *io)
 {
@@ -19,39 +63,13 @@ int	ft_redirect(t_io *io)
 	while (io)
 	{
 		if (io->type == IO_IN)
-		{
-			fd = open(io->file, O_RDONLY);
-			if (fd == -1)
-				panic("open");
-			dup2(fd, STDIN_FILENO);
-			close(fd);
-		}
+			redirect_in(io);
 		else if (io->type == IO_OUT)
-		{
-			fd = open(io->file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if (fd == -1)
-				panic("open");
-			dup2(fd, STDOUT_FILENO);
-			close(fd);
-		}
+			redirect_out(io);
 		else if (io->type == IO_APPEND)
-		{
-			fd = open(io->file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-			if (fd == -1)
-				panic("open");
-			dup2(fd, STDOUT_FILENO);
-			close(fd);
-		}
+			redirect_append(io);
 		else if (io->type == IO_HEREDOC)
-		{
-			fd = 0;
-			// open a file to read the heredoc content
-			// fd = open(".heredoc", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-			if (fd == -1)
-				panic("open");
-			dup2(fd, STDIN_FILENO);
-			close(fd);
-		}
+			redirect_heredoc(io);
 		io = io->next;
 	}
 	return (0);
