ce8f940625144c1466f5cb4893869c4c8a6db5fe
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2024-04-05 06:10:02 +0000

    fix norme

--- src/exec/exec_simple_cmd.c
+++ src/exec/exec_simple_cmd.c
@@ -1,30 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   exec_simple_cmd.c                                  :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/30 10:52:43 by yaharkat          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2024/04/05 07:29:43 by obenchkr         ###   ########.fr       */
-=======
-/*   Updated: 2024/04/05 06:07:48 by obenchkr         ###   ########.fr       */
->>>>>>> 702bd1a (fix norme)
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../../includes/minishell.h"
 
-<<<<<<< HEAD
-=======
 bool	is_redirection_node(t_node *tree)
 {
 	return (tree->io && (tree->io->type == IO_IN || tree->io->type == IO_OUT
 			|| tree->io->type == IO_APPEND || tree->io->type == IO_HEREDOC));
 }
 
->>>>>>> 702bd1a (fix norme)
 static int	ft_exec_cmd(t_node *tree, char **args)
 {
 	int		status;
@@ -37,12 +18,8 @@ static int	ft_exec_cmd(t_node *tree, char **args)
 		panic("fork");
 	if (pid == 0)
 	{
-<<<<<<< HEAD
-		if (tree->io)
-=======
 
 		if (is_redirection_node(tree))
->>>>>>> 702bd1a (fix norme)
 			ft_redirect(tree->io);
 		if (!cmd->path)
 		{
@@ -55,8 +32,7 @@ static int	ft_exec_cmd(t_node *tree, char **args)
 			exit(1);
 		}
 	}
-	waitpid(pid, &status, 0);
-	return (WEXITSTATUS(status));
+	return (waitpid(pid, &status, 0), WEXITSTATUS(status));
 }
 
 int	ft_exec_simple_cmd(t_node *tree, bool is_pipe)
