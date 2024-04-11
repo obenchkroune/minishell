84a1d0be5a85468149fbe766eac4476d4e9b9614
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2024-04-11 18:17:25 +0000

    fix get_next_line memory leaks

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -3,105 +3,92 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: oussama <oussama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 21:59:16 by yaharkat          #+#    #+#             */
-/*   Updated: 2024/03/09 22:56:58 by obenchkr         ###   ########.fr       */
+/*   Updated: 2024/04/11 18:08:15 by oussama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-char	*get_clean_line(char *buffer)
+static ssize_t	read_from_buffer(int fd, char **history)
 {
-	int	i;
-
-	if (!buffer || !*buffer)
-		return (NULL);
-	i = 0;
-	while (buffer[i] != '\n' && buffer[i] != '\0')
-		i++;
-	return (ft_substr(buffer, 0, i + 1));
-}
-
-char	*get_rest(char *s)
-{
-	char	*rest;
-	int		i;
-	int		len;
-
-	i = 0;
-	if (!s[i])
-	{
-		free(s);
-		return (NULL);
-	}
-	while (s[i] && s[i] != '\n')
-		i++;
-	if (s[i] && s[i] == '\n')
-		i++;
-	len = ft_strlen(&s[i]) + 1;
-	rest = (char *)malloc(sizeof(char) * len);
-	ft_strlcpy(rest, s + i, len);
-	free(s);
-	return (rest);
-}
-
-char	*initisalize_buffer(void)
-{
+	ssize_t	bytes;
 	char	*buffer;
+	char	*temp;
 
-	if (BUFFER_SIZE <= 0)
-		return (NULL);
-	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (!buffer)
-		return (NULL);
-	buffer[0] = '\0';
-	return (buffer);
+	buffer = malloc(BUFFER_SIZE + 1);
+	ft_memset(buffer, 0, BUFFER_SIZE + 1);
+	bytes = read(fd, buffer, BUFFER_SIZE);
+	if (bytes <= 0)
+	{
+		free(buffer);
+		return (bytes);
+	}
+	temp = *history;
+	*history = ft_strjoin(*history, buffer);
+	free(temp);
+	free(buffer);
+	return (bytes);
 }
 
-char	*read_line_lines(char *buffer, char *line, int fd, ssize_t *bytes_read)
+static void	cleanup_history(char **history)
 {
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	char	*nl;
+	char	*temp;
+
+	nl = ft_strchr(*history, '\n');
+	temp = *history;
+	*history = ft_strdup(nl + 1);
+	free(temp);
+}
+
+static char	*get_result(char **history)
+{
+	char	*nl;
+	char	*result;
+	ssize_t	i;
+
+	nl = ft_strchr(*history, '\n');
+	if (!nl)
 	{
-		free(buffer);
-		return (NULL);
+		result = ft_strdup(*history);
+		free(*history);
+		*history = NULL;
+		return (result);
 	}
-	*bytes_read = read(fd, line, BUFFER_SIZE);
-	if (*bytes_read < 0)
+	result = malloc(nl - *history + 2);
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < nl - *history + 1)
 	{
-		free(buffer);
-		free(line);
-		return (NULL);
+		result[i] = (*history)[i];
+		i++;
 	}
-	line[*bytes_read] = '\0';
-	buffer = ft_strjoin(buffer, line);
-	return (buffer);
+	result[i] = '\0';
+	cleanup_history(history);
+	return (result);
 }
 
 char	*get_next_line(int fd)
 {
-	char		*line[2];
-	static char	*buffer[1024];
-	ssize_t		bytes_read;
+	static char	*history;
+	ssize_t		bytes;
 
-	line[0] = initisalize_buffer();
-	if (line[0] == NULL)
+	if (BUFFER_SIZE <= 0 || fd < 0)
 		return (NULL);
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (history == NULL)
+		history = ft_strdup("");
+	bytes = 1;
+	while (bytes > 0 && !!history && !ft_strchr(history, '\n'))
+		bytes = read_from_buffer(fd, &history);
+	if (bytes < 0 || ft_strlen(history) == 0)
 	{
-		free(line[0]);
+		free(history);
+		history = NULL;
 		return (NULL);
 	}
-	bytes_read = 1;
-	while (bytes_read > 0 && !ft_strchr(line[0], '\n'))
-	{
-		buffer[fd] = read_line_lines(buffer[fd], line[0], fd, &bytes_read);
-		if (!buffer[fd])
-			return (NULL);
-	}
-	line[1] = get_clean_line(buffer[fd]);
-	buffer[fd] = get_rest(buffer[fd]);
-	free(line[0]);
-	return (line[1]);
+	return (get_result(&history));
 }
