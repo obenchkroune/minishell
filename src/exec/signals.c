c8ac3cdd557939fe5161d8875b6231551a20803f
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2024-04-11 18:29:31 +0000

    set the last exit status after catching a `SIGINT`

--- src/exec/signals.c
+++ src/exec/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaharkat <yaharkat@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: oussama <oussama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 06:45:05 by yaharkat          #+#    #+#             */
-/*   Updated: 2024/04/08 00:22:39 by yaharkat         ###   ########.fr       */
+/*   Updated: 2024/04/11 18:28:37 by oussama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 void	sigint_handler(int signum)
 {
-	(void)signum;
 	ft_fprintf(STDOUT_FILENO, "\n");
 	rl_replace_line("", 0);
 	if (!g_shell->input)
+	{
 		rl_on_new_line();
+		g_shell->last_exit_status = signum + 128;
+	}
 	if (!g_shell->inside_unclosed_pipe)
 		rl_redisplay();
 	else
