38ae364127ace31570b92e46d544025dc990fdee
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2024-04-12 17:52:52 +0000

    `ft_strjoin`: fix memory leaks on NULL arg

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: oussama <oussama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 18:25:29 by yaharkat          #+#    #+#             */
-/*   Updated: 2024/03/25 06:26:03 by obenchkr         ###   ########.fr       */
+/*   Updated: 2024/04/12 16:00:39 by oussama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ char	*ft_strjoin(char *s1, char *s2)
 	if (!s1 && !s2)
 		return (NULL);
 	if (!s1)
-		s1 = ft_strdup("");
+		s1 = "";
 	if (!s2)
-		s2 = ft_strdup("");
+		s2 = "";
 	size = ft_strlen(s1) + ft_strlen(s2);
 	dst = (char *)malloc(sizeof(char) * (size + 1));
 	if (!dst)
