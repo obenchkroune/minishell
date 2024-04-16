03fc6f4063d740eac5202d15e6386a5938023ac4
obenchkr <obenchkr@student.1337.ma>
2024-04-16 04:45:11 +0100

    remove static functions

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 05:10:23 by obenchkr          #+#    #+#             */
-/*   Updated: 2024/04/16 04:03:42 by obenchkr         ###   ########.fr       */
+/*   Updated: 2024/04/16 04:45:02 by obenchkr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,40 +18,6 @@
 #include <stdlib.h>
 
 t_shell		*g_shell;
-
-static char	*get_user_host_line(void)
-{
-	char	*result;
-	size_t	len;
-	size_t	color_len;
-
-	color_len = ft_strlen(GREEN) + ft_strlen(RESET);
-	len = color_len + ft_strlen(g_shell->username)
-		+ ft_strlen(g_shell->hostname) + 3;
-	result = ft_calloc(len + 1, sizeof(char));
-	if (!result)
-		panic("malloc");
-	ft_strlcat(result, CYAN, len + 1);
-	ft_strlcat(result, g_shell->username, len + 1);
-	ft_strlcat(result, "@", len + 1);
-	ft_strlcat(result, g_shell->hostname, len + 1);
-	ft_strlcat(result, RESET ": ", len + 1);
-	return (result);
-}
-
-static char	*get_display_line(void)
-{
-	char	*temp;
-	char	*display_line;
-	char	*user_host_line;
-
-	user_host_line = get_user_host_line();
-	temp = ft_strjoin(user_host_line, g_shell->cwd);
-	free(user_host_line);
-	display_line = ft_strjoin(temp, "\n\r$ ");
-	free(temp);
-	return (display_line);
-}
 
 static void	g_shell_checks_update(void)
 {
