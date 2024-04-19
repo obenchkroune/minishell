6ad29ca1c7d834860a6b4ad148ebf2ccd878f655
obenchkr <obenchkr@student.1337.ma>
2024-04-19 17:14:28 +0100

    Update `TODO.md`

--- TODO.md
+++ TODO.md
@@ -1,13 +1,3 @@
 # TODO
 
 - [ ] Fix leaks when aborting quote prompt
-
-- [ ] Fix SEGV on execution when using redirection without a command
-
-	- [ ] `> out`
-	
-	- [ ] `< in`
-	
-	- [ ] `<< EOL`
-	
-	- [ ] `>> OK`
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 05:10:23 by obenchkr          #+#    #+#             */
-/*   Updated: 2024/04/19 17:12:55 by obenchkr         ###   ########.fr       */
+/*   Updated: 2024/04/19 17:14:07 by obenchkr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,6 @@ int	main(int argc, char **argv, char **envp)
 		cleanup_rotation();
 		display_prompt();
 		parse_input();
-		// if (g_shell->tree)
-		// 	printf("tree cmd: %p\n", g_shell->tree->cmd);
 		ft_exec_node(g_shell->tree, false);
 	}
 	return (EXIT_SUCCESS);
