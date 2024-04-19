edd0fa69bb7308c9472780c5b7ca4198953c577b
obenchkr <obenchkr@student.1337.ma>
2024-04-19 11:52:52 +0100

    fix norm

--- src/utils/syntax_error.c
+++ src/utils/syntax_error.c
@@ -6,14 +6,11 @@
 /*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 04:59:02 by obenchkr          #+#    #+#             */
-/*   Updated: 2024/04/19 11:34:47 by obenchkr         ###   ########.fr       */
+/*   Updated: 2024/04/19 11:50:55 by obenchkr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-
-#define SYNTAX_ERROR \
-RED "minishell: " RESET "syntax error near unexpected token `%s'\n"
 
 void	syntax_error(char *msg)
 {
@@ -24,7 +21,9 @@ void	syntax_error(char *msg)
 	else
 	{
 		token = get_next_token();
-		ft_fprintf(2, SYNTAX_ERROR, token.value);
+		ft_fprintf(STDERR_FILENO,
+			RED "minishell: " RESET "syntax error near unexpected token `%s'\n",
+			token.value);
 		if (token.type == T_WORD)
 			free(token.value);
 	}
