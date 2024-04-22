ca5dba4a13e399bb5356b0b555c6b336ae22d174
obenchkr <obenchkr@student.1337.ma>
2024-04-22 22:56:01 +0100

    zsh

--- src/utils/get_display_line.c
+++ src/utils/get_display_line.c
@@ -6,7 +6,7 @@
 /*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 04:26:18 by obenchkr          #+#    #+#             */
-/*   Updated: 2024/04/22 21:49:26 by obenchkr         ###   ########.fr       */
+/*   Updated: 2024/04/22 22:53:24 by obenchkr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,16 @@
 char	*get_display_line(void)
 {
 	char	*display_line;
+	char	*folder;
 	char	*temp;
 
-	temp = ft_strjoin("📁 ", g_shell->cwd);
-	display_line = ft_strjoin(temp, GREEN "\n➜  " RESET);
+	folder = ft_strrchr(g_shell->cwd, '/');
+	if (folder)
+		display_line = ft_strjoin(GREEN "➜  " RESET "\e[1m" CYAN, folder + 1);
+	else
+		display_line = ft_strjoin(GREEN "➜  " RESET "\e[1m" CYAN, g_shell->cwd);
+	temp = display_line;
+	display_line = ft_strjoin(display_line, " \e[m" RESET);
 	free(temp);
 	return (display_line);
 }
