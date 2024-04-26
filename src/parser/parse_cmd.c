a378a95fa8f2394059de9df9bdc704aa707dd431
OUSSAMA BENCHKRON SAID <obenchkr@student.1337.ma>
2024-04-26 15:32:39 +0100

    check if the file is a directory when searching for the executable

--- src/parser/parse_cmd.c
+++ src/parser/parse_cmd.c
@@ -11,12 +11,6 @@
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
-#include "libft.h"
-#include <fcntl.h>
-#include <stdbool.h>
-#include <sys/stat.h>
-#include <time.h>
-#include <unistd.h>
 
 t_redir_type	get_redir_type(t_token token)
 {
@@ -36,15 +30,18 @@ t_redir_type	get_redir_type(t_token token)
 	}
 }
 
-static bool	is_regular_file(const char *file)
+static bool	check_file(const char *file)
 {
 	int			fd;
 	struct stat	file_info;
 
+	if (access(file, F_OK | R_OK) != 0)
+		return (false);
 	fd = open(file, O_RDONLY);
 	if (fstat(fd, &file_info) < 0)
-		return (close(fd), true);
-	return (close(fd), !S_ISDIR(file_info.st_mode));
+		return (close(fd), false);
+	close(fd);
+	return (S_ISREG(file_info.st_mode));
 }
 
 char	*get_executable(char *cmd)
@@ -57,22 +54,23 @@ char	*get_executable(char *cmd)
 	i = 0;
 	result = NULL;
 	cmd = ft_expand(cmd);
-	if (!is_regular_file(cmd) || !get_env("PATH"))
+	if (!get_env("PATH"))
 		return (free(cmd), NULL);
-	if (access(cmd, X_OK | F_OK) == 0)
+	if (check_file(cmd))
 		return (cmd);
 	path = ft_split(get_env("PATH"), ':');
 	slash_cmd = ft_strjoin("/", cmd);
 	while (path && path[i])
 	{
 		result = ft_strjoin(path[i], slash_cmd);
-		if (access(result, X_OK | F_OK) == 0)
+		if (check_file(result))
 			break ;
 		free(result);
 		result = NULL;
 		i++;
 	}
-	return (free(slash_cmd), free_tab(path), free(cmd), result);
+	(free(slash_cmd), free_tab(path), free(cmd));
+	return (result);
 }
 
 t_node	*parse_cmd(void)
