a45beb0bf10a0285e9280d504757ae0ce2d1078c
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2024-06-16 14:34:42 +0100

    feat: change prompt color based on last exit status

--- src/utils/get_display_line.c
+++ src/utils/get_display_line.c
@@ -6,7 +6,7 @@
 /*   By: obenchkr <obenchkr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 04:26:18 by obenchkr          #+#    #+#             */
-/*   Updated: 2024/06/14 11:58:03 by obenchkr         ###   ########.fr       */
+/*   Updated: 2024/06/16 14:23:36 by obenchkr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@ char	*get_display_line(void)
 	char	*display_line;
 	char	*temp;
 	char	*cwd;
+	char	*minishell;
 
+	minishell = CYAN COLOR_BOLD "minishell: " COLOR_OFF RESET;
+	if (g_shell->last_exit.status != 0)
+		minishell = RED COLOR_BOLD "minishell: " COLOR_OFF RESET;
 	cwd = ft_strreplace(g_shell->cwd, g_shell->home, "~");
-	temp = ft_strjoin(CYAN COLOR_BOLD "minishell: " COLOR_OFF RESET, cwd);
+	temp = ft_strjoin(minishell, cwd);
 	display_line = ft_strjoin(temp, "$ ");
 	(free(cwd), free(temp));
 	return (display_line);
